95379141e3d198f6083dd0a37604ce0f6283cbdf
aagneye-syam <saagneye2003@gmail.com>
2025-12-29 21:05:48 +0530

    Add survival time and user image to global store

--- lib/store.ts
+++ lib/store.ts
@@ -4,8 +4,12 @@ import { persist } from 'zustand/middleware'
 interface AppState {
   username: string | null
   fanLevel: number | null
+  survivalTime: number | null
+  userImage: string | null
   setUsername: (username: string) => void
   setFanLevel: (level: number) => void
+  setSurvivalTime: (time: number) => void
+  setUserImage: (image: string | null) => void
   reset: () => void
   clearFanLevel: () => void
 }
@@ -15,10 +19,14 @@ export const useAppStore = create<AppState>()(
     (set) => ({
       username: null,
       fanLevel: null,
+      survivalTime: null,
+      userImage: null,
       setUsername: (username) => set({ username }),
       setFanLevel: (fanLevel) => set({ fanLevel }),
-      reset: () => set({ username: null, fanLevel: null }),
-      clearFanLevel: () => set({ fanLevel: null }),
+      setSurvivalTime: (survivalTime) => set({ survivalTime }),
+      setUserImage: (userImage) => set({ userImage }),
+      reset: () => set({ username: null, fanLevel: null, survivalTime: null, userImage: null }),
+      clearFanLevel: () => set({ fanLevel: null, survivalTime: null }),
     }),
     {
       name: 'ethmumbai-storage',
